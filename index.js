26eca3203362b971b81d13f70d953f35f5657f2b
Blake Pearson <blake.pearson159@gmail.com>
2020-06-30 14:55:51 -0700

    working on postal calc

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ const PORT = process.env.PORT || 5000
     var type = requestUrl.query.type;
 
     getRate(res, type, weight);
-  })
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  });
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
   function getRate(res, type, weight)
   {
